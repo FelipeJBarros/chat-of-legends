4ccab97f7712d2e501ae8bc99c88b3fd16cd8e7e
Felipe Barros <felipebarros.engh@gmail.com>
2024-03-28 18:35:13 -0300

    feat: initial page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,28 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'chat-of-legends';
+
+  contacts = [
+    {
+      id: 1,
+      name: "Poppy",
+      role: "Tank",
+      lore: "Yordle portadora do martelo",
+      imageUrl: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Poppy_0.jpg"
+    },
+    {
+      id: 2,
+      name: "Ornn",
+      role: "Tank",
+      lore: "Deus da forja de Frejord",
+      imageUrl: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ornn_0.jpg"
+    },
+    {
+      id: 3,
+      name: "Vladmir",
+      role: "Mago",
+      lore: "Vampiro hemomante de Noxus",
+      imageUrl: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Vladmir_0.jpg"
+    },
+  ]
 }
